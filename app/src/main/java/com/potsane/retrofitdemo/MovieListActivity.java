9e6164e8c5f39a15240359af1a55807f2bbcc089
potsane <potsanemohale@gmail.com>
2018-06-13 13:43:38 +0200

    -Adding initial project

---
+++ app/src/main/java/com/potsane/retrofitdemo/MovieListActivity.java
@@ -0,0 +1,13 @@
+package com.potsane.retrofitdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MovieListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_list);
+    }
+}
