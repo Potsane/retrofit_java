4fa7b4bc537fbbe072ea977dc994422ea76a8994
potsane <potsanemohale@gmail.com>
2018-06-13 14:43:46 +0200

    -Adding the MovieList view model class ana getTopmovies method

--- app/src/main/java/com/potsane/retrofitdemo/network/ApiAccess.java
+++ app/src/main/java/com/potsane/retrofitdemo/network/ApiAccess.java
@@ -10,12 +10,12 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiAccess {
 
     private static Retrofit retrofit = null;
-    public static final String BASE_URL = "http://api.themoviedb.org/3/";
+    public static final String URL = "http://api.themoviedb.org/3/";
 
-    public static Retrofit getClient() {
-        if (retrofit==null) {
+    public static Retrofit createRetrofitInstance() {
+        if (retrofit == null) {
             retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
+                    .baseUrl(URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
--- app/src/main/java/com/potsane/retrofitdemo/view/MovieListViewModel.java
+++ app/src/main/java/com/potsane/retrofitdemo/view/MovieListViewModel.java
@@ -1,8 +1,20 @@
 package com.potsane.retrofitdemo.view;
 
+import android.arch.lifecycle.ViewModel;
+
+import com.potsane.retrofitdemo.entity.Movie;
+
+import java.util.List;
+
 /**
  * Created by PMohale on 2018/06/13.
  */
 
-public class MovieListViewModel {
+public class MovieListViewModel extends ViewModel {
+
+    public List<Movie> getTopMovies(String apiKey) {
+        return null;
+    }
 }
+
+
